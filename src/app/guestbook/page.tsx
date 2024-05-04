1f3fbb3ddc0d8d429722ed4d55d3336369ce608a
ahmad <itxti909@gmail.com>
2024-05-04 22:30:35 +0300

    kinde auth

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -1,45 +1,47 @@
-import { Button } from '@/components/ui/button'
-import { Card, CardHeader } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import React from "react";
 
-function GuestBookForm() {
-    const user = false
+async function GuestBookForm() {
+    const {getUser}= getKindeServerSession();
+    const user = await getUser();
 
-    if (user) {
-        return <div>
-            <h1>Hello</h1>
-        </div>
-    }
+  if (user) {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 
-    return <div className='flex justify-between gap-4 flex-col md:flex-row'>
-        <Input type='text' placeholder='Your Message...'/>
-        <Button>
-            Sign For Free
-        </Button>
+  return (
+    <div className="flex justify-between gap-4 flex-col md:flex-row">
+      <Input type="text" placeholder="Your Message..." />
+      <RegisterLink>
+        <Button>Sign For Free</Button>
+      </RegisterLink>
     </div>
+  );
 }
-
 
 const GuestBookPage = () => {
   return (
-    <section className='max-w-7xl w-full px-4 md:px-8 mx-auto'>
-        <h1 className='text-4xl font-semibold lg:text-5xl'>
-            GuestBook
-        </h1>
-        <p className='leading-7 text-muted-foreground mt-2'>Sign My GuestBook!</p>
+    <section className="max-w-7xl w-full px-4 md:px-8 mx-auto">
+      <h1 className="text-4xl font-semibold lg:text-5xl">GuestBook</h1>
+      <p className="leading-7 text-muted-foreground mt-2">Sign My GuestBook!</p>
 
-        <Card className='mt-10'>
-            <CardHeader className='flex flex-col w-full'>
-                <Label className='mb-1'>
-                    Message
-                </Label>
-                <GuestBookForm />
-            </CardHeader>
-        </Card>
+      <Card className="mt-10">
+        <CardHeader className="flex flex-col w-full">
+          <Label className="mb-1">Message</Label>
+          <GuestBookForm />
+        </CardHeader>
+      </Card>
     </section>
-  )
-}
+  );
+};
 
-export default GuestBookPage
+export default GuestBookPage;
